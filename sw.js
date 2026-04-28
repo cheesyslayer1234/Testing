fdcf758f311ef781d3d42fa99d44e3430910a266
2401779J <2401779J@student.tp.edu.sg>
2026-04-28 21:27:10 +0800

    Sjsj

--- sw.js
+++ sw.js
@@ -1,18 +1,27 @@
 self.addEventListener("install", () => {
-    self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-    event.waitUntil(self.clients.claim());
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("message", (event) => {
-    console.log("SW received:", event.data);
+let timer = null;
 
-    if (event.data?.type === "NOTIFY") {
-        self.registration.showNotification(event.data.title, {
-            body: event.data.body,
-            requireInteraction: true
-        });
-    }
+self.addEventListener("message", (event) => {
+  console.log("SW received:", event.data);
+
+  if (event.data?.type === "START_TIMER") {
+    const delay = event.data.delay || 10;
+
+    // clear existing timer if any
+    if (timer) clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      self.registration.showNotification("TIMER DONE", {
+        body: "This is Pomofocus-style SW notification",
+        requireInteraction: true
+      });
+    }, delay * 1000);
+  }
 });
