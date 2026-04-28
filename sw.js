ae0866a80377bb80ca3c8f3d7d19bd37516fb9ee
cheesyslayer1234 <2401779J@student.tp.edu.sg>
2026-04-28 21:00:50 +0800

    w

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
     self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-    self.clients.claim();
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("message", (event) => {
     if (event.data?.type === "NOTIFY") {
         self.registration.showNotification(event.data.title, {
-            body: event.data.body,
-            icon: "https://cdn-icons-png.flaticon.com/512/1827/1827392.png"
+            body: event.data.body
         });
     }
 });
