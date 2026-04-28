26370df36985083ffd2e9507fa46f76da86fc42a
cheesyslayer1234 <2401779J@student.tp.edu.sg>
2026-04-28 21:11:30 +0800

    WHY

--- sw.js
+++ sw.js
@@ -12,8 +12,7 @@ self.addEventListener("message", (event) => {
     if (event.data?.type === "NOTIFY") {
         self.registration.showNotification(event.data.title, {
             body: event.data.body,
-            icon: "https://cdn-icons-png.flaticon.com/512/1827/1827392.png",
-            requireInteraction: false
+            requireInteraction: true
         });
     }
 });
